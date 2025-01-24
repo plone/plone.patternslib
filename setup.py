52e0e3cb703bcb7cc7f9f224bbdae2f8772b45bf
ale-rt <alessandro.pisa@gmail.com>
2025-01-24 15:59:27 +0100

    pre-commit run --all black

--- setup.py
+++ setup.py
@@ -4,16 +4,18 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-long_description = '\n\n'.join([
-    open('README.rst').read(),
-    open('CONTRIBUTORS.rst').read(),
-    open('CHANGES.rst').read(),
-])
+long_description = "\n\n".join(
+    [
+        open("README.rst").read(),
+        open("CONTRIBUTORS.rst").read(),
+        open("CHANGES.rst").read(),
+    ]
+)
 
 
 setup(
-    name='plone.patternslib',
-    version='2.0.0.dev0',
+    name="plone.patternslib",
+    version="2.0.0.dev0",
     description="An add-on to make patternslib patterns available within Plone 5.",  # noqa
     long_description=long_description,
     # See https://pypi.org/classifiers/
@@ -34,25 +36,25 @@ setup(
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
-    keywords='Python Plone Patternslib',
-    author='Syslab.com',
-    author_email='pilz@syslab.com',
-    url='https://pypi.python.org/pypi/plone.patternslib',
-    license='GPL version 2',
-    packages=find_packages('src', exclude=['ez_setup']),
-    namespace_packages=['plone'],
-    package_dir={'': 'src'},
+    keywords="Python Plone Patternslib",
+    author="Syslab.com",
+    author_email="pilz@syslab.com",
+    url="https://pypi.python.org/pypi/plone.patternslib",
+    license="GPL version 2",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    namespace_packages=["plone"],
+    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires='>=3.9',
+    python_requires=">=3.9",
     install_requires=[
-        'setuptools',
-        'Products.CMFPlone',
+        "setuptools",
+        "Products.CMFPlone",
     ],
     extras_require={
-        'test': [
-            'plone.app.testing',
-            'plone.app.robotframework[debug]',
+        "test": [
+            "plone.app.testing",
+            "plone.app.robotframework[debug]",
         ],
     },
     entry_points="""
--- src/plone/patternslib/__init__.py
+++ src/plone/patternslib/__init__.py
@@ -3,4 +3,4 @@
 from zope.i18nmessageid import MessageFactory
 
 
-_ = MessageFactory('plone.patternslib')
+_ = MessageFactory("plone.patternslib")
