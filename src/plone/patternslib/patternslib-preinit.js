050cc04e8a27f82f67f4e4bb91ba4d2d15f4e827
Johannes Raggam <thetetet@gmail.com>
2025-09-10 11:40:02 +0200

    Comment: Patternslib' pat-modal is actually not a duplicate of Mockup's version. Mockup uses plone-modal and pat-plone-modal as name and trigger.

--- src/plone/patternslib/patternslib-preinit.js
+++ src/plone/patternslib/patternslib-preinit.js
@@ -6,7 +6,6 @@
 window.__patternslib_patterns_blacklist = [
     "legend", // Transforms <legend> to <p>, breaks pat-autotoc form tabbing.
     // Duplicates
-    //"modal",
     //"sortable",
     //"toggle",
 ];
