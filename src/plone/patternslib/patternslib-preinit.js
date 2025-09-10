8849a3986d55d2c625efabee3151a26f215cd9ce
Johannes Raggam <thetetet@gmail.com>
2025-09-10 11:43:52 +0200

    Comment about blacklisting duplicates.

--- src/plone/patternslib/patternslib-preinit.js
+++ src/plone/patternslib/patternslib-preinit.js
@@ -6,6 +6,8 @@
 window.__patternslib_patterns_blacklist = [
     "legend", // Transforms <legend> to <p>, breaks pat-autotoc form tabbing.
     // Duplicates
+    // These patterns are loaded after the Mockup versions, so they are not
+    // initialized and do not need to be blacklisted.
     //"sortable",
     //"toggle",
 ];
